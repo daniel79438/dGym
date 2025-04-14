ed1d526ee9396b0c05663da4c173b796eddc3fb1
daniel79438 <daniel79438@gmail.com>
2025-04-14 15:37:23 +0100

    Initial Commit

---
+++ dGym/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dGym
+//
+//  Created by Daniel Harris on 14/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dGym/dGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  dGymApp.swift
+//  dGym
+//
+//  Created by Daniel Harris on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct dGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
